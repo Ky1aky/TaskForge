a9ed77de7bafd48dbd55f9d1bf75a2432d17bc9a
Ian Christopher Villegas <kyia1nvillegas@gmail.com>
2026-06-27 16:14:01 +0800

    Add project files.

---
+++ TaskForge/Data/TaskForgeDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TaskForge.Data
+{
+    public class TaskForgeDbContext : DbContext
+    {
+        public TaskForgeDbContext(DbContextOptions<TaskForgeDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
